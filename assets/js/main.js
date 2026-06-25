e48b6f62ea77116ccb73be22c5b6a1c964980060
Looffeplush <looffeplush@gmail.com>
2026-06-25 15:41:06 +0800

    Launch Looffe Web 2.0 MVP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Forty by HTML5 UP
+	Massively by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -10,22 +10,26 @@
 		$body = $('body'),
 		$wrapper = $('#wrapper'),
 		$header = $('#header'),
-		$banner = $('#banner');
+		$nav = $('#nav'),
+		$main = $('#main'),
+		$navPanelToggle, $navPanel, $navPanelInner;
 
 	// Breakpoints.
-	breakpoints({
-		xlarge:    ['1281px',   '1680px'   ],
-		large:     ['981px',    '1280px'   ],
-		medium:    ['737px',    '980px'    ],
-		small:     ['481px',    '736px'    ],
-		xsmall:    ['361px',    '480px'    ],
-		xxsmall:   [null,       '360px'    ]
-	});
+		breakpoints({
+			default:   ['1681px',   null       ],
+			xlarge:    ['1281px',   '1680px'   ],
+			large:     ['981px',    '1280px'   ],
+			medium:    ['737px',    '980px'    ],
+			small:     ['481px',    '736px'    ],
+			xsmall:    ['361px',    '480px'    ],
+			xxsmall:   [null,       '360px'    ]
+		});
 
 	/**
 	 * Applies parallax scrolling to an element's background image.
+	 * @return {jQuery} jQuery object.
 	 */
-	$.fn._parallax = (browser.name == 'ie' || browser.name == 'edge' || browser.mobile) ? function() { return $(this) } : function(intensity) {
+	$.fn._parallax = function(intensity) {
 
 		var	$window = $(window),
 			$this = $(this);
@@ -34,210 +38,221 @@
 			return $this;
 
 		if (this.length > 1) {
+
 			for (var i=0; i < this.length; i++)
 				$(this[i])._parallax(intensity);
+
 			return $this;
+
 		}
 
 		if (!intensity)
 			intensity = 0.25;
 
 		$this.each(function() {
+
 			var $t = $(this),
+				$bg = $('<div class="bg"></div>').appendTo($t),
 				on, off;
 
 			on = function() {
-				$t.css('background-position', 'center 100%, center 100%, center 0px');
-				$window.on('scroll._parallax', function() {
-					var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
-					$t.css('background-position', 'center ' + (pos * (-1 * intensity)) + 'px');
-				});
+
+				$bg
+					.removeClass('fixed')
+					.css('transform', 'matrix(1,0,0,1,0,0)');
+
+				$window
+					.on('scroll._parallax', function() {
+
+						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+
+						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
+
+					});
+
 			};
 
 			off = function() {
-				$t.css('background-position', '');
-				$window.off('scroll._parallax');
+
+				$bg
+					.addClass('fixed')
+					.css('transform', 'none');
+
+				$window
+					.off('scroll._parallax');
+
 			};
 
-			breakpoints.on('<=medium', off);
-			breakpoints.on('>medium', on);
+			// Disable parallax on ..
+				if (browser.name == 'ie'			// IE
+				||	browser.name == 'edge'			// Edge
+				||	window.devicePixelRatio > 1		// Retina/HiDPI (= poor performance)
+				||	browser.mobile)					// Mobile devices
+					off();
+
+			// Enable everywhere else.
+				else {
+
+					breakpoints.on('>large', on);
+					breakpoints.on('<=large', off);
+
+				}
+
 		});
 
-		$window.off('load._parallax resize._parallax')
+		$window
+			.off('load._parallax resize._parallax')
 			.on('load._parallax resize._parallax', function() {
 				$window.trigger('scroll');
 			});
 
 		return $(this);
+
 	};
 
 	// Play initial animations on page load.
-	$window.on('load', function() {
-		window.setTimeout(function() {
-			$body.removeClass('is-preload');
-		}, 100);
-	});
-
-	// Clear transitioning state on unload/hide.
-	$window.on('unload pagehide', function() {
-		window.setTimeout(function() {
-			$('.is-transitioning').removeClass('is-transitioning');
-		}, 250);
-	});
-
-	// Fix: Enable IE-only tweaks.
-	if (browser.name == 'ie' || browser.name == 'edge')
-		$body.addClass('is-ie');
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
 	// Scrolly.
-	$('.scrolly').scrolly({
-		offset: function() {
-			return $header.height() - 2;
-		}
-	});
+		$('.scrolly').scrolly();
 
-	// Tiles.
-	var $tiles = $('.tiles > article');
+	// Background.
+		$wrapper._parallax(0.925);
 
-	$tiles.each(function() {
-		var $this = $(this),
-			$image = $this.find('.image'), $img = $image.find('img'),
-			$link = $this.find('.link'),
-			x;
+	// Nav Panel.
 
-		// Image.
-		$this.css('background-image', 'url(' + $img.attr('src') + ')');
-		if (x = $img.data('position'))
-			$image.css('background-position', x);
-		$image.hide();
+		// Toggle.
+			$navPanelToggle = $(
+				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
+			)
+				.appendTo($wrapper);
 
-		// Link.
-		if ($link.length > 0) {
-			$x = $link.clone().text('').addClass('primary').appendTo($this);
-			$link = $link.add($x);
+			// Change toggle styling once we've scrolled past the header.
+				$header.scrollex({
+					bottom: '5vh',
+					enter: function() {
+						$navPanelToggle.removeClass('alt');
+					},
+					leave: function() {
+						$navPanelToggle.addClass('alt');
+					}
+				});
 
-			$link.on('click', function(event) {
-				var href = $link.attr('href');
-				event.stopPropagation();
-				event.preventDefault();
-				if ($link.attr('target') == '_blank') {
-					window.open(href);
-				} else {
-					$this.addClass('is-transitioning');
-					$wrapper.addClass('is-transitioning');
-					window.setTimeout(function() {
-						location.href = href;
-					}, 500);
+		// Panel.
+			$navPanel = $(
+				'<div id="navPanel">' +
+					'<nav>' +
+					'</nav>' +
+					'<a href="#navPanel" class="close"></a>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-navPanel-visible'
+				});
+
+			// Get inner.
+				$navPanelInner = $navPanel.children('nav');
+
+			// Move nav content on breakpoint change.
+				var $navContent = $nav.children();
+
+				breakpoints.on('>medium', function() {
+
+					// NavPanel -> Nav.
+						$navContent.appendTo($nav);
+
+					// Flip icon classes.
+						$nav.find('.icons, .icon')
+							.removeClass('alt');
+
+				});
+
+				breakpoints.on('<=medium', function() {
+
+					// Nav -> NavPanel.
+						$navContent.appendTo($navPanelInner);
+
+					// Flip icon classes.
+						$navPanelInner.find('.icons, .icon')
+							.addClass('alt');
+
+				});
+
+			// Hack: Disable transitions on WP.
+				if (browser.os == 'wp'
+				&&	browser.osVersion < 10)
+					$navPanel
+						.css('transition', 'none');
+
+	// Intro.
+		var $intro = $('#intro');
+
+		if ($intro.length > 0) {
+
+			// Hack: Fix flex min-height on IE.
+				if (browser.name == 'ie') {
+					$window.on('resize.ie-intro-fix', function() {
+
+						var h = $intro.height();
+
+						if (h > $window.height())
+							$intro.css('height', 'auto');
+						else
+							$intro.css('height', h);
+
+					}).trigger('resize.ie-intro-fix');
 				}
+
+			// Hide intro on scroll (> small).
+				breakpoints.on('>small', function() {
+
+					$main.unscrollex();
+
+					$main.scrollex({
+						mode: 'bottom',
+						top: '25vh',
+						bottom: '-50vh',
+						enter: function() {
+							$intro.addClass('hidden');
+						},
+						leave: function() {
+							$intro.removeClass('hidden');
+						}
+					});
+
+				});
+
+			// Hide intro on scroll (<= small).
+				breakpoints.on('<=small', function() {
+
+					$main.unscrollex();
+
+					$main.scrollex({
+						mode: 'middle',
+						top: '15vh',
+						bottom: '-15vh',
+						enter: function() {
+							$intro.addClass('hidden');
+						},
+						leave: function() {
+							$intro.removeClass('hidden');
+						}
+					});
+
 			});
+
 		}
-	});
-
-	// Header.
-	if ($banner.length > 0 && $header.hasClass('alt')) {
-
-		$window.on('resize', function() { $window.trigger('scroll'); });
-
-		$window.on('load', function() {
-			$banner.scrollex({
-				bottom: $header.height() + 10,
-				terminate: function() { $header.removeClass('alt'); },
-				enter: function() { $header.addClass('alt'); },
-				leave: function() { $header.removeClass('alt'); $header.addClass('reveal'); }
-			});
-			window.setTimeout(function() { $window.triggerHandler('scroll'); }, 100);
-		});
-	}
-
-	// Banner.
-	$banner.each(function() {
-		var $this = $(this),
-			$image = $this.find('.image'), $img = $image.find('img');
-
-		$this._parallax(0.275);
-
-		if ($image.length > 0) {
-			$this.css('background-image', 'url(' + $img.attr('src') + ')');
-			$image.hide();
-		}
-	});
-
-	// Menu.
-	var $menu = $('#menu'),
-		$menuInner;
-
-	$menu.wrapInner('<div class="inner"></div>');
-	$menuInner = $menu.children('.inner');
-	$menu._locked = false;
-
-	$menu._lock = function() {
-		if ($menu._locked) return false;
-		$menu._locked = true;
-		window.setTimeout(function() { $menu._locked = false; }, 350);
-		return true;
-	};
-
-	$menu._show = function() { if ($menu._lock()) $body.addClass('is-menu-visible'); };
-	$menu._hide = function() { if ($menu._lock()) $body.removeClass('is-menu-visible'); };
-	$menu._toggle = function() { if ($menu._lock()) $body.toggleClass('is-menu-visible'); };
-
-	$menuInner.on('click', function(event) { event.stopPropagation(); })
-		.on('click', 'a', function(event) {
-			var href = $(this).attr('href');
-			event.preventDefault();
-			event.stopPropagation();
-			$menu._hide();
-			window.setTimeout(function() { window.location.href = href; }, 250);
-		});
-
-	$menu.appendTo($body)
-		.on('click', function(event) {
-			event.stopPropagation();
-			event.preventDefault();
-			$body.removeClass('is-menu-visible');
-		})
-		.append('<a class="close" href="#menu">Close</a>');
-
-	$body.on('click', 'a[href="#menu"]', function(event){
-		event.stopPropagation();
-		event.preventDefault();
-		$menu._toggle();
-	}).on('click', function(event){ $menu._hide(); })
-	  .on('keydown', function(event){ if(event.keyCode == 27) $menu._hide(); });
 
 })(jQuery);
-
-/* -----------------------------------
-   Lightbox 功能 - jQuery 版本
------------------------------------ */
-$(document).ready(function(){
-
-	var $lightbox = $("#lightbox"),
-		$lightboxImg = $("#lightbox-img");
-
-	// 点击缩略图打开 lightbox
-	$(".thumb img").off("click").on("click", function(e){
-		e.stopPropagation(); // 阻止冒泡
-		var src = $(this).attr("data-large");
-		$lightboxImg.attr("src", src);
-		$lightbox.fadeIn();
-	});
-
-	// 点击背景或关闭按钮关闭 lightbox
-	$lightbox.add(".lightbox .close").off("click").on("click", function(e){
-		if(e.target === this || $(e.target).hasClass("close")){
-			$lightbox.fadeOut();
-			// 清空 src 避免内存占用或旧图闪烁
-			setTimeout(function(){ $lightboxImg.attr("src", ""); }, 200);
-		}
-	});
-
-	// ESC 关闭
-	$(document).on("keydown", function(e){
-		if(e.key === "Escape" && $lightbox.is(":visible")){
-			$lightbox.fadeOut();
-			setTimeout(function(){ $lightboxImg.attr("src",""); }, 200);
-		}
-	});
-
-});
